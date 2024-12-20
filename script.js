6fa4a4998fef30481b29dc0075ae71a6230e5a3b
Lidiomar <lidiomarmillaalves@gmail.com>
2024-12-20 07:47:49 -0300

    Procurando candidato + voto nulo.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let descricao = document.querySelector('.d-1-4');
 let aviso = document.querySelector('.d-2');
 let lateral = document.querySelector('.d-1-right');
 let numeros = document.querySelector('.d-1-3');
+let numeroDigitado = '';
 
 let etapaAtual = 0;
 
@@ -28,17 +29,35 @@ function comecarEtapa() {
     numeros.innerHTML = numeroHtml;
 }
 function atualizaInterface() {
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item) => item.numero === numeroDigitado);
 
+    if (candidato.length > 0) {
+        candidato = candidato[0];
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
+
+        let fotosHtml = '';
+        for (let i in candidato.fotos) {
+            fotosHtml += `<div class="d-1-image"><img src="images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
+        }
+        lateral.innerHTML = fotosHtml;
+    } else {
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = `<div class="aviso--grande pisca">Voto Nulo</div>`;
+    }
 }
 
 function clicou(n) {
     let elNumero = document.querySelector('.numero.pisca');
     if (elNumero !== null) {
         elNumero.innerHTML = n;
-        numeros = `${numeros}${n}`;
+        numeroDigitado += n;
 
         elNumero.classList.remove('pisca');
-        if(elNumero.nextElementSibling !== null) {
+        if (elNumero.nextElementSibling !== null) {
             elNumero.nextElementSibling.classList.add('pisca');
         } else {
             atualizaInterface();
